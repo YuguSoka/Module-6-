bda53154f3cd3e6835d4ce14605afea5bbe5b2ad
YuguSoka <yugusuk.soka@gmail.com>
2024-11-26 12:43:20 +0800

    Add files via upload

---
+++ Lab 1/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Greeting from './components/Greeting'
+
+function App() { 
+
+  return (
+    <>
+      <Greeting name="Bruce Wayne">
+        <div>Welcome to Gotham City.</div>
+      </Greeting>
+    </>
+  )
+}
+
+export default App
